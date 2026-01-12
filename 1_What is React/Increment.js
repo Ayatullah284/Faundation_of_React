920a6eec639187260ea3004d595f5e7c36c87d27
Ayatullah <ayatullah284@gmail.com>
2026-01-12 08:12:50 +0600

    What is React and How it works

--- 1_What is React/Increment.js
+++ 1_What is React/Increment.js
@@ -13,6 +13,47 @@ const domContainer = document.querySelector('#root');
 // ReactDOM.render(myElement, domContainer);
 
 
-let p = document.createElement('p');
+/* let p = document.createElement('p');
 p.innerHTML = 'Hello world';
-domContainer.appendChild(p);
+domContainer.appendChild(p); */
+
+const Increment = () => {
+    const [counter, setCounter] = React.useState(0);
+    // console.log(reactState);
+    return(
+        <div>
+            <h1 id="display">{counter}</h1>
+            <div>
+                <button id="button" onClick={() => setCounter(counter + 1)}>Increment +</button>
+            </div>
+        </div>
+    )
+}
+
+/* const myElement = (
+    <div>
+        <h1 id="display">0</h1>
+        <div>
+            <button id="button">Increment +</button>
+        </div>
+    </div>
+); */
+
+ReactDOM.render(
+    <div className="container">
+        <Increment />
+        <Increment />
+        <Increment />
+    </div>, 
+    domContainer
+);
+
+/* let number = 0;
+
+const button = document.querySelector('#button');
+const display = document.querySelector('#display');
+
+button.addEventListener('click', () => {
+    number++ ;
+    display.textContent = number;
+}); */
