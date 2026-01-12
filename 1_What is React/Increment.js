0d4e2c4405b7f38a2d12b84a0e64bb68ca92348a
Ayatullah <ayatullah284@gmail.com>
2026-01-12 07:35:51 +0600

    what is React

---
+++ 1_What is React/Increment.js
@@ -0,0 +1,13 @@
+/* console.log('React',React);
+console.log('ReactDOM', ReactDOM); */
+
+
+const domContainer = document.querySelector('#root');
+
+const myElement = React.createElement('div', null, [
+    React.createElement('p', null, 'Hello world'),
+    React.createElement('p', null, 'Hello world'),
+]);
+
+
+ReactDOM.render(myElement, domContainer);
