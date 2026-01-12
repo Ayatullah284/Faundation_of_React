19a259da4ab959f84c473b2ffb145208f47f4238
Ayatullah <ayatullah284@gmail.com>
2026-01-12 07:45:14 +0600

    domContainer

--- 1_What is React/Increment.js
+++ 1_What is React/Increment.js
@@ -4,10 +4,15 @@ console.log('ReactDOM', ReactDOM); */
 
 const domContainer = document.querySelector('#root');
 
-const myElement = React.createElement('div', null, [
+/* const myElement = React.createElement('div', null, [
     React.createElement('p', null, 'Hello world'),
     React.createElement('p', null, 'Hello world'),
-]);
+]); */
 
 
-ReactDOM.render(myElement, domContainer);
+// ReactDOM.render(myElement, domContainer);
+
+
+let p = document.createElement('p');
+p.innerHTML = 'Hello world';
+domContainer.appendChild(p);
